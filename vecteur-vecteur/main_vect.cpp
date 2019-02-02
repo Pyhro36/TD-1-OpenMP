ee7a90151fac8c003179296381f0a9ad17054734
Pierre-Louis <pllefebvre3@gmail.com>
2019-02-02 16:15:55 +0100

    with comments

--- vecteur-vecteur/main_vect.cpp
+++ vecteur-vecteur/main_vect.cpp
@@ -9,9 +9,11 @@ int main(int argc, char **argv)
 {
     int i;
 
+    // instanciation des vecteurs
     int *vector1 = new int[NB_MAX];
     int *vector2 = new int[NB_MAX];
 
+    // remplissage des vecteurs
     srand(time(NULL));
 
     for (i = 0; i < NB_MAX; i++)
@@ -19,7 +21,8 @@ int main(int argc, char **argv)
         vector1[i] = rand();
         vector2[i] = rand();
     }
-   
+
+    // liberation memoire
     delete[] vector1;
     delete[] vector2;
 
